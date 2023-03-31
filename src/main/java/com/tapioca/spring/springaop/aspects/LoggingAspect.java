aa20902926db1ad5f26e91abb025d53676c31562
RyouHikaru <carljamesreblora13@gmail.com>
2023-03-31 18:23:10 +0800

    Initial commit

---
+++ src/main/java/com/tapioca/spring/springaop/aspects/LoggingAspect.java
@@ -0,0 +1,21 @@
+package com.tapioca.spring.springaop.aspects;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
+@Aspect
+public class LoggingAspect {
+
+	@Before("execution(* com.tapioca.spring.springaop.ProductServiceImpl.multiply(..))")
+	public void logBefore(JoinPoint joinPoint) {
+		System.out.println("Before calling the method");
+	}
+
+	@After("execution(* com.tapioca.spring.springaop.ProductServiceImpl.multiply(..))")
+	public void logAfter(JoinPoint joinPoint) {
+		System.out.println("After the method execution");
+	}
+
+}
